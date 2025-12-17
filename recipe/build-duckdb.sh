757a60cdab9b7a31f8317da44e718f3aaa70ea55
Serhii Kupriienko <skupriienko@anaconda.com>
2025-12-17 16:15:07 +0200

    Update recipe

--- recipe/build-duckdb.sh
+++ recipe/build-duckdb.sh
@@ -2,16 +2,24 @@
 
 set -euxo pipefail
 
+echo ${SRC_DIR}
+ls -la ${SRC_DIR}
+
 if [[ "$target_platform" == "linux-ppc64le" ]]; then
   # avoid error 'relocation truncated to fit: R_PPC64_REL24'
   export CFLAGS="$(echo ${CFLAGS} | sed 's/-fno-plt//g') -fplt"
   export CXXFLAGS="$(echo ${CXXFLAGS} | sed 's/-fno-plt//g') -fplt"
 fi
 
-if [[ "$target_platform" == "linux-aarch64" ]]; then
-  # linux_aarch64 with the jemalloc extension has trouble finding pthread symbols
+if [[ "$target_platform" == "linux-aarch64" || "$target_platform" == "linux-ppc64le" ]]; then
+  # jemalloc extension on aarch64/ppc64le needs pthread symbols
   export CFLAGS="${CFLAGS} -pthread"
   export CXXFLAGS="${CXXFLAGS} -pthread"
+  export JEMALLOC_SYS_WITH_LG_PAGE=16
 fi
 
-${PYTHON} -m pip install --no-deps --no-build-isolation tools/pythonpkg -vv
+
+# export OVERRIDE_GIT_DESCRIBE=v$PKG_VERSION-0-gb8a06e4
+export CMAKE_GENERATOR=Ninja
+
+${PYTHON} -m pip install --no-deps --no-build-isolation -vv .
