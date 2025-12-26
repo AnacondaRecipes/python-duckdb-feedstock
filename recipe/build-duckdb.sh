9516bb4cf354120247a7a05c41ec2b8675850981
Serhii Kupriienko <skupriienko@anaconda.com>
2025-12-26 10:46:38 +0200

    Fix build scripts

--- recipe/build-duckdb.sh
+++ recipe/build-duckdb.sh
@@ -2,9 +2,6 @@
 
 set -euxo pipefail
 
-echo ${SRC_DIR}
-ls -la ${SRC_DIR}
-
 if [[ "$target_platform" == "linux-ppc64le" ]]; then
   # avoid error 'relocation truncated to fit: R_PPC64_REL24'
   export CFLAGS="$(echo ${CFLAGS} | sed 's/-fno-plt//g') -fplt"
